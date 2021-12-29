977f3a9562269c59eb2ed61ca1db0a1a6568e6c6
dante18 <lablandinerie2@gmail.com>
2021-12-29 15:14:18 +0100

    Addition of the component used to display the content of the default page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponentsComponent } from './components/home-components/home-components.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentsComponent
   ],
   imports: [
     BrowserModule,
