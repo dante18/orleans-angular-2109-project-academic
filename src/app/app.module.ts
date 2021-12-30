07563eeec8f20fb4328bb42f12a5f46901d25c75
dante18 <lablandinerie2@gmail.com>
2021-12-30 14:37:47 +0100

    é

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponentsComponent } from './components/home-components/home-components.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponentsComponent
+    HomeComponentsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
