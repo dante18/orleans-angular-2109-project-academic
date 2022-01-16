75d6571d6af657d3f7b2c7eefef78e5dc16ded52
dante18 <lablandinerie2@gmail.com>
2022-01-16 16:25:42 +0100

    Add files used to tests application

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const apiConfig = require("./api/config/config");
 const app = express();
+const sequelizeFixtures = require('sequelize-fixtures');
 
 /* initialization link to database with Squelize */
 const db = require("./api/models");
 if (apiConfig.environment === "dev") {
-  db.cnx.sync({ force: true }).then(() => {
+  db.cnx.sync({force: true}).then(() => {
     console.log("Drop and re-sync db.");
+
+    /* Loading data to test the application during the development phase. */
+    sequelizeFixtures.loadFile('fixtures/*.json', db).then(() => {
+      console.log('Seed data used to tests application loaded!');
+    });
   });
 } else {
   db.cnx.sync();
@@ -23,11 +29,11 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
-// simple route
+// default route
 app.get("/api/status", (request, response) => {
-  response.json({ message: "API is UP" });
+  response.json({message: "API ngFormation"});
 });
 
 // import routes
