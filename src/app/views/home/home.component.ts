4abd7d35213c9df9a4468f001eef2fc89d3aac38
dante18 <lablandinerie2@gmail.com>
2022-01-25 20:47:36 +0100

    Update home page

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormationService} from "../../services/formation.service";
+import {Formation} from "../../models/formation";
+import {InternService} from "../../services/intern.service";
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  numberTotalOfFormation = 0;
+  numberTotalOfIntern = 0;
+  formationList: Formation[] = [];
 
-  constructor() { }
+  constructor(private serviceFormation: FormationService, private serviceIntern: InternService) { }
 
   ngOnInit(): void {
+    this.getNotificationData();
   }
 
+  getNotificationData()
+  {
+    this.serviceFormation.findAllFormation().subscribe({
+      next: (value) => {
+        value.forEach((formation:any) => {
+          if (formation.isAvailable) {
+            this.formationList.push(formation);
+            this.numberTotalOfFormation += 1;
+          }
+        })
+      },
+      error: (error) => {
+        console.log("The data could not be retrieved. Error invoked : " + error.message)
+      },
+      complete: () => {
+        console.log("The data has been successfully recovered");
+      }
+    });
+
+    this.serviceIntern.findAll().subscribe({
+      next: (value) => {
+        value.forEach((intern:any) => {
+          this.numberTotalOfIntern += 1
+        })
+      },
+      error: (error) => {
+        console.log("The data could not be retrieved. Error invoked : " + error.message)
+      },
+      complete: () => {
+        console.log("The data has been successfully recovered");
+      }
+    });
+  }
 }
