7c6351c4dd2b82247e2c16125625f18591c990a6
dante18 <lablandinerie2@gmail.com>
2022-02-05 18:06:10 +0100

    Update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,25 +16,25 @@ import {AddInternComponent} from "./views/intern/add-intern/add-intern.component
 import {EditInternComponent} from "./views/intern/edit-intern/edit-intern.component";
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: '/accueil', pathMatch: 'full'},
+  {path: 'accueil', component: HomeComponent},
 
   {path: 'formation', component: FormationComponent},
-  {path: 'formation/add', component: AddFormationComponent},
-  {path: 'formation/detail/:id', component: DetailFormationComponent},
-  {path: 'formation/edit/:id', component: EditFormationComponent},
+  {path: 'formation/ajouter', component: AddFormationComponent},
+  {path: 'formation/consulter/:id', component: DetailFormationComponent},
+  {path: 'formation/modifier/:id', component: EditFormationComponent},
 
-  {path: 'category', component: CategoryComponent},
-  {path: 'category/add', component: AddCategoryComponent},
-  {path: 'category/edit/:id', component: EditCategoryComponent},
+  {path: 'categorie', component: CategoryComponent},
+  {path: 'categorie/ajouter', component: AddCategoryComponent},
+  {path: 'categorie/edit/:id', component: EditCategoryComponent},
 
-  {path: 'former', component: FormerComponent},
-  {path: 'former/add', component: AddFormerComponent},
-  {path: 'former/edit/:id', component: EditFormerComponent},
+  {path: 'formateur', component: FormerComponent},
+  {path: 'formateur/ajouter', component: AddFormerComponent},
+  {path: 'formateur/modifier/:id', component: EditFormerComponent},
 
-  {path: 'intern', component: InternComponent},
-  {path: 'intern/add', component: AddInternComponent},
-  {path: 'intern/edit/:id', component: EditInternComponent},
+  {path: 'stagiaire', component: InternComponent},
+  {path: 'stagiaire/ajouter', component: AddInternComponent},
+  {path: 'stagiaire/modifier/:id', component: EditInternComponent},
 ];
 
 @NgModule({
